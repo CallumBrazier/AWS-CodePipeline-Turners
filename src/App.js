b7c8b0f1db2c1db52fe3d5cd7b2dd396e7006ca1
Callum Brazier <braziercallum@gmail.com>
2021-02-17 13:37:06 +1300

    additional styling for homepage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
 import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPhoneAlt,
+  faTaxi,
+  faSearch,
+  faBars,
+  faPortrait,
+} from "@fortawesome/free-solid-svg-icons";
 
 import logo from "./images/turners-logo.png";
 import logo2 from "./images/logo-transparent.png";
@@ -7,8 +15,22 @@ function App() {
   return (
     <div className="App">
       <body>
-        <header>
-          <img src={logo2} alt="turners-logo" className="logo" />
+        <header className="header">
+          <div className="header-left">
+            <img src={logo2} alt="turners-logo" className="logo" />
+            <p className="phone">
+              <FontAwesomeIcon icon={faPhoneAlt} /> 0800 887 637
+            </p>
+          </div>
+          <div className="header-left">
+            <FontAwesomeIcon icon={faTaxi} className="taxi" />
+            <button className="login-buttons">
+              Login <FontAwesomeIcon icon={faPortrait} />
+            </button>
+            <button className="login-buttons">Join us</button>
+
+            <FontAwesomeIcon icon={faBars} className="burger-bars" />
+          </div>
         </header>
       </body>
     </div>
